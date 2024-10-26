a8a261f34dcc8c75e436930b565236e563e76e17
Jake Gillman <jakegillman3@gmail.com>
2024-10-26 12:23:02 +0100

    Initial commit

---
+++ SirenStudio/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SirenStudio;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ SirenStudio/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SirenStudio;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}

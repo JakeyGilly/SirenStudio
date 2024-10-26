d0ed1fc0f234ab66875af712ffd7e17ffefb8836
the-bald-lad <samjbloomfield@btinternet.com>
2024-10-26 13:53:52 +0100

    Started main page

--- SirenStudio/MainPage.xaml.cs
+++ SirenStudio/MainPage.xaml.cs
@@ -1,32 +1,84 @@
-﻿namespace SirenStudio;
+﻿using System.Diagnostics.CodeAnalysis;
 
-public partial class MainPage : ContentPage
+namespace SirenStudio;
+
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public partial class MainPage
 {
-    private int count;
-    private bool m_pressed = true;
+    private string m_project_name = string.Empty;
+    private string m_project_path = string.Empty; 
+    
+    private static readonly FilePickerFileType s_custom_file_type = new FilePickerFileType(
+        new Dictionary<DevicePlatform, IEnumerable<string>>
+        {
+            { DevicePlatform.WinUI, [".srn"] },
+            { DevicePlatform.macOS, [".srn"] }, 
+        });
+
+    private readonly PickOptions m_options = new()
+    {
+        PickerTitle = "Please select a project",
+        FileTypes = s_custom_file_type,
+    };
 
     public MainPage()
     {
         InitializeComponent();
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
+    private async void OpenExplorerWindow(object sender, EventArgs e)
     {
-        count++;
-        CounterBtn.Text = $"Clicked {count} time" + (count == 1 ? "" : "s");
+        try
+        {
+            var result = await FilePicker.Default.PickAsync(m_options);
+            if (result != null)
+            {
+                m_project_path = result.ToString() ?? string.Empty;
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            throw;
+        }
     }
 
-    private void OnMusicClicked(object sender, EventArgs e)
+    private void CheckFullPath()
     {
-        if (m_pressed)
+        if (m_project_name != string.Empty && m_project_path != string.Empty)
         {
-            OtherButton.Text = $"I lied";
-            m_pressed = false;
+            PathLabel.Text = $"Project will be created in: {m_project_path}\\{m_project_name}";
         }
-        else
-        {
-            OtherButton.Text = $"Press for even more music";
-            m_pressed = true;
-        }
+    }
+
+    private void OnNameTextChanged(object sender, TextChangedEventArgs e)
+    {
+        m_project_name = e.NewTextValue;
+        CheckFullPath();
+    }
+
+    private void OnNameTextCompleted(object sender, EventArgs e)
+    {
+        m_project_name = ((Entry)sender).Text;
+        CheckFullPath();
+    }
+
+    private void OnPathTextChanged(object sender, TextChangedEventArgs e)
+    {
+        m_project_path = e.NewTextValue;
+        CheckFullPath();
+    }
+
+    private void OnPathCompleted(object sender, EventArgs e)
+    {
+        m_project_path = ((Entry)sender).Text;
+        CheckFullPath();
+    }
+
+    private void CreateFolder(object? sender, EventArgs e)
+    {
+        // Create folder with file
     }
 }
